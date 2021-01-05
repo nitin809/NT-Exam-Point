edb88e6a6fc83dbe9a80c01b275a42d972e85b02
nitin809 <55194700+nitin809@users.noreply.github.com>
2021-01-05 15:44:17 +0530

    Add files via upload

---
+++ 01_while_syntax.py
@@ -0,0 +1,8 @@
+# WHILE LOOP SYNATAX
+
+a = 0
+while a < 5:  # The block or while loop keeps executing until the condition is true!
+    print("yes " + "the value of a is " + str(a))
+    a = a + 1
+
+print("loop ended")
---
+++ 02_quiz_whileloop.py
@@ -0,0 +1,16 @@
+# WRITE A PROGRAM TO PRINT 1 TO 50 USING WHILE LOOP
+
+# a = 1
+
+# while a <=50:
+#     print(a)
+#     a = a+1
+
+# print "Harry" five times
+
+
+i = 0
+
+while i<5:
+    print("Harry")
+    i = i + 1
